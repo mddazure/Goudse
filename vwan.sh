29e550ae77393763bff18699b460428a026feb7f
Marc de Droog <madedroo@microsoft.com>
2024-05-03 17:24:02 +0200

    scriptupd

--- vwan.sh
+++ vwan.sh
@@ -1,15 +1,15 @@
-$rgname = "Goudse1"
-$location = "swedencentral"
+rgname="Goudse1"
+location="swedencentral"
 
-az group create -n $rgname -l swedencentral
+az group create -n $rgname -l $location
 
-az network vwan create -g $rgname -l swedencentral -n vwan-0
-az network vwan create -g $rgname -l swedencentral -n vwan-1
+az network vwan create -g $rgname -l $location -n vwan-0
+az network vwan create -g $rgname -l $location -n vwan-1
 
 
-az network vhub create -g $rgname -n hub-0 -l swedencentral --addressprefix 192.168.0.0/24 --vwan-0 --no-wait
-az network vhub create -g $rgname -n hub-2 -l swedencentral --addressprefix 192.168.2.0/24 --vwan-0 --no-wait
-az network vhub create -g $rgname -n hub-1 -l swedencentral --addressprefix 192.168.1.0/24 --vwan-1 --no-wait
+az network vhub create -g $rgname -n hub-0 -l $location --addressprefix 192.168.0.0/24 --vwan-0 --no-wait
+az network vhub create -g $rgname -n hub-2 -l $location --addressprefix 192.168.2.0/24 --vwan-0 --no-wait
+az network vhub create -g $rgname -n hub-1 -l $location --addressprefix 192.168.1.0/24 --vwan-1 --no-wait
 
 az network vhub route-table create --name RED -g $rgname --vhub-name hub-0 --labels red
 
